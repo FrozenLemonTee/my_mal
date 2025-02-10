ce1eff139ef4009cdfbc7cd4d59639c7115c4a69
frozenlemontee <frozenlemontee@gmail.com>
2025-02-10 21:42:18 +0800

    feat: Add more types

--- impls/cxx/types.cpp
+++ impls/cxx/types.cpp
@@ -1,7 +1,12 @@
 #include "types.h"
 
+#include <iomanip>
 #include <regex>
 #include <utility>
+
+auto MalType::isKeyword(const std::string &token) -> bool {
+    return token[0] == ':';
+}
 
 auto MalType::isInt(const std::string& token) -> bool {
     return std::regex_match(token, std::regex("^[-+]?[0-9]{1,19}$"));
@@ -15,12 +20,34 @@ auto MalType::isBool(const std::string& token) -> bool {
     return token == "true" || token == "false";
 }
 
-auto MalType::isChar(const std::string& token) -> bool {
-    return token.size() == 1;
+auto MalType::isString(const std::string &token) -> bool {
+    if (token.length() < 2) {
+        return false;
+    }
+
+    if (token[0] != '\"' || token[token.length() - 1] != '\"') {
+        return false;
+    }
+
+    const std::string content = token.substr(1, token.length() - 2);
+
+    for (size_t i = 0; i < content.length(); ++i) {
+        if (content[i] == '\\') {
+            if (i == content.length() - 1 ||
+                (content[i + 1] != 'n' && content[i + 1] != 't' && content[i + 1] != '\\' &&
+                 content[i + 1] != '\"' && content[i + 1] != 'r')) {
+                return false;
+                 }
+            ++i;
+        }
+    }
+
+    return true;
 }
 
+
 auto MalNil::to_string() -> std::string {
-    return {};
+    return "nil";
 }
 
 MalBool::MalBool(const bool val) : val_(val) {}
@@ -35,10 +62,33 @@ auto MalInt::to_string() -> std::string {
     return std::to_string(this->val_);
 }
 
-MalChar::MalChar(const char val) : val_(val) {}
+MalChar::MalChar(const std::string& val) {
+    const std::string charStr = val.substr(1, val.length() - 2);
+
+    if (charStr == "\\n") {
+        this->val_ = '\n';
+    } else if (charStr == "\\t") {
+        this->val_ = '\t';
+    } else if (charStr == "\\r") {
+        this->val_ = '\r';
+    } else if (charStr == "\\\'") {
+        this->val_ = '\'';
+    } else if (charStr == "\\\"") {
+        this->val_ = '\"';
+    } else if (charStr == "\\\\") {
+        this->val_ = '\\';
+    }
+    this->val_ = charStr[0];
+}
 
 auto MalChar::to_string() -> std::string {
     return std::to_string(this->val_);
+}
+
+MalString::MalString(const std::string& val) : val_(std::move(val.substr(1, val.length() - 2))) {}
+
+auto MalString::to_string() -> std::string {
+    return "\"" + val_ + "\"";
 }
 
 MalSymbol::MalSymbol(std::string name) : name_(std::move(name)) {}
@@ -55,11 +105,11 @@ auto MalList::to_string() -> std::string {
     ss << "(";
     bool first = true;
     for (const auto element : elements_) {
-        ss << element->to_string();
-        if (first) {
+        if (!first) {
             ss << " ";
-            first = false;
         }
+        ss << element->to_string();
+        first = false;
     }
     ss << ")";
     return ss.str();
@@ -68,5 +118,62 @@ auto MalList::to_string() -> std::string {
 MalList::~MalList() {
     for (const auto e: this->elements_) {
         delete e;
+    }
+}
+
+MalVector::MalVector(std::vector<MalType *> elements)
+    : elements_(std::move(elements)) {}
+
+auto MalVector::to_string() -> std::string {
+    std::stringstream ss;
+    ss << "[";
+    bool first = true;
+    for (const auto element : elements_) {
+        if (!first) {
+            ss << " ";
+        }
+        ss << element->to_string();
+        first = false;
+    }
+    ss << "]";
+    return ss.str();
+}
+
+MalVector::~MalVector() {
+    for (const auto e: this->elements_) {
+        delete e;
+    }
+}
+
+MalKeyword::MalKeyword(std::string name)
+    : name_(std::move(name)) {}
+
+auto MalKeyword::to_string() -> std::string {
+    return ":" + this->name_;
+}
+
+
+MalMap::MalMap(const std::map<MalType*, MalType*>& elements)
+    : elements_(elements) {}
+
+auto MalMap::to_string() -> std::string {
+    std::stringstream ss;
+    ss << "{";
+    bool first = true;
+    for (const auto [key, val] : elements_) {
+        if (!first) {
+            ss << " ";
+        }
+        ss << key->to_string() << " " << val->to_string();
+        first = false;
+    }
+    ss << "}";
+    return ss.str();
+}
+
+MalMap::~MalMap() {
+    for (const auto e: this->elements_) {
+        delete e.first;
+        delete e.second;
     }
 }
--- impls/cxx/types.h
+++ impls/cxx/types.h
@@ -1,27 +1,29 @@
 #ifndef TYPES_H
 #define TYPES_H
+#include <map>
 #include <string>
 #include <vector>
 
 class MalType {
     public:
+        static bool isKeyword(const std::string& token);
         static bool isInt(const std::string& token);
         static bool isNil(const std::string& token);
         static bool isBool(const std::string& token);
-        static bool isChar(const std::string& token);
+        static bool isString(const std::string& token);
 
-        virtual ~MalType() = 0;
+        virtual ~MalType() = default;
         virtual std::string to_string() = 0;
 };
 
 class MalAtom : public MalType {
     public:
-        ~MalAtom() override = 0;
+        ~MalAtom() override = default;
 };
 
 class MalStruct : public MalType {
     public:
-        ~MalStruct() override = 0;
+        ~MalStruct() override = default;
 };
 
 class MalNil final : public MalAtom {
@@ -46,7 +48,14 @@ class MalInt final : public MalAtom {
 class MalChar final : public MalAtom {
         char val_;
     public:
-        explicit MalChar(char val);
+        explicit MalChar(const std::string& val);
+        std::string to_string() override;
+};
+
+class MalString final : public MalAtom {
+        std::string val_;
+    public:
+        explicit MalString(const std::string&  val);
         std::string to_string() override;
 };
 
@@ -61,10 +70,37 @@ class MalList final : public MalStruct {
         std::vector<MalType*> elements_;
 
     public:
-    explicit MalList(std::vector<MalType*> elements);
+        explicit MalList(std::vector<MalType*> elements);
+
+        std::string to_string() override;
+        ~MalList() override;
+};
+
+class MalVector final : public MalStruct {
+        std::vector<MalType*> elements_;
+    public:
+        explicit MalVector(std::vector<MalType*> elements);
+
+        std::string to_string() override;
+        ~MalVector() override;
+};
+
+class MalKeyword final : public MalAtom {
+        std::string name_;
+    public:
+        explicit MalKeyword(std::string name);
+
+        std::string to_string() override;
+        ~MalKeyword() override = default;
+};
+
+class MalMap final : public MalStruct {
+    std::map<MalType*, MalType*> elements_;
+    public:
+    explicit MalMap(const std::map<MalType*, MalType*>& elements);
 
     std::string to_string() override;
-    ~MalList() override;
+    ~MalMap() override;
 };
 
 #endif //TYPES_H
